ac541ac60b85afceb7fcad21f223c6e62d02cc61
Andrew Mfune <andiemfune@gmail.com>
2019-10-03 13:58:31 -0800

    Using a dedicated function for submitting responses and checking reference code

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -21,50 +21,72 @@ class ApiController extends AppController
         $this->Auth->allow(['authenticate', 'challenge', 'submission']);
     }
 
+    private function _submit($request, $response){
+        $this->loadModel('Participants');
+        try{
+            $request = $request->data;
+            if (array_key_exists('code', $request)){
+                $code = $request['code'];
+                Log::write('debug',__('Processing request for {0} with data: {1}', $code, json_encode($request)));
+                if($this->Participants->verify($code)){
+                    Log::write('debug', __('Request ok!, response {0}', json_encode($response)));
+                    echo json_encode($response);
+                    return;
+                }
+            }
+            Log::write('debug', __('Invalid reference code! Unable to process request {0}. ', json_encode($request)));
+            $this->response->statusCode(404);
+        }catch(\Exception $error){
+            Log::write('debug', __('Error occured during request {0}: {1}', json_encode($request), $error->getMessage()));
+            echo json_encode(['error' => 'An error has occured while processing request']);
+        }
+    }
+
     public function authenticate(){
         $this->loadModel('Participants');
         if ($this->request->is('post')) 
         {
-            $response = [];
-            $data = $this->request->data;
-            if ($this->Participants->verify($data['code'])){
-                $response = $this->Participants->getAllFromRefCode($data['code']);
-            }else{
-                $response = ['error'=>'Reference code does not exist'];
-            }
+            Log::write('debug', 'Authenticating reference code...');
+            $this->_submit(
+                $this->request, 
+                $this->Participants->getAllFromRefCode(
+                    $this->request->data['code']
+                )
+            );
         }
-        echo json_encode($response);
     }
 
     public function challenge(){
-        $this->loadModel('Challenges');
-        $response = [];
-        $activeChallenge = $this->Challenges->getActiveChallenge();
-        if ($activeChallenge){
-            $response = [
-                "id" => $activeChallenge->id,
-                "title" => $activeChallenge->title,
-                "limit" => $activeChallenge->time_limit,
-                "passage" => $activeChallenge->paragraph
-            ];
-        }else{
-            $response = ['error' => 'No challenge available at the momment!'];
+        Log::write('debug', 'Requesting active challenge...');
+        if ($this->request->is('post')){
+            $this->loadModel('Challenges');
+            $response = [];
+            $activeChallenge = $this->Challenges->getActiveChallenge();
+            if ($activeChallenge){
+                $this->_submit($this->request, [
+                    "id" => $activeChallenge->id,
+                    "title" => $activeChallenge->title,
+                    "limit" => $activeChallenge->time_limit,
+                    "passage" => $activeChallenge->paragraph
+                ]);
+            }else{
+                $this->_submit($this->request, ['error' => 'No challenge available at the momment!']);
+            }
         }
-        echo json_encode($response);
     }
 
     public function submission(){
+        Log::write('debug', 'Submitting scores..');
         $response = [];
         $this->loadModel('Perfomances');
         if ($this->request->is('post')){
             $entity = $this->Perfomances->log($this->request->data);
             if ($entity){
                 $this->response->statusCode('201');
-                $response = ['score' =>  $entity->score];
+                $this->_submit($this->request, ['score' =>  $entity->score]);
             }else{
-                $response = ['error' => 'An error has occured while saving score data'];
+                $this->_submit($this->request, ['error' => 'An error has occured while saving score data']);
             }
         }
-        echo json_encode($response);
     }
 }
